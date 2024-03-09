751c4c9b68f525ad971df634204ad3183cf33947
fukenrice <63079551+fukenrice@users.noreply.github.com>
2024-03-09 22:35:40 +0300

    Init

---
+++ app/src/main/java/com/example/products_app/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.example.products_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.products_app.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
